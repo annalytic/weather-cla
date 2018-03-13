872163f558d53b987629fd4a34e4a58bf14591db
Anna <halloannali@gmail.com>
2018-03-13 15:07:37 +0100

    added api credit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # weather-cla
 Weather Command Line Application written in Node.js
+
+API: https://www.wunderground.com/
--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+// Requires
 const weather = require('./weather.js');
 
+// Query
 const query = process.argv.slice(2).join("_").replace(' ', '_');
 
 weather.get(query);
